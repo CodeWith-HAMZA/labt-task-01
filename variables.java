3523aaec5f0bde150b2be585208edb5fe594a2d2
Irfanaliqureshi <114615634+Irfanaliqureshi@users.noreply.github.com>
2022-10-10 07:21:59 +0500

    Update variables.java

--- variables.java
+++ variables.java
@@ -1,27 +1,18 @@
 class variables {
 public static void main(String[] args){
-int a=1;
-int b=2;
-int c=3;
-int d=4;
+int a=1,b=2,c=3,d=4;
 System.out.println( "INTEGER VARIABLES");
 System.out.println("here a variable store integer datatype "+a);
 System.out.println("here b variable store integer datatype "+b);
 System.out.println("here c variable store integer datatype "+c);
 System.out.println("here d variable store integer datatype "+d);
-double e=1.1;
-double f=2.2;
-double g=3.3;
-double h=4.4;
+double e=1.1,f=2.2,g=3.3,h=4.4;
 System.out.println( "FLOAT VARIABLES");
 System.out.println("here e variable store float datatype "+e);
 System.out.println("here f variable store float datatype "+f);
 System.out.println("here g variable store float datatype "+g);
 System.out.println("here h variable store float datatype "+h);
-char m='A';
-char n='B';
-char o='C';
-char p='D';
+char m='A',n='B',o='C',p='D';
 System.out.println( "CHARACTER  VARIABLES");		
 System.out.println("here m variable store character datatype "+m);
 System.out.println("here n variable store character datatype "+n);
